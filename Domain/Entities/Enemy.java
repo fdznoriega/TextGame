0738a35d07045a6a56c1c678022db634018ba5af
fdznoriega <andresfdz7@gmail.com>
2019-09-03 17:12:36 -0400

    Added enemy class, updated roadmap

---
+++ Domain/Entities/Enemy.java
@@ -0,0 +1,9 @@
+package Domain.Entities;
+
+import Domain.Entities.Stats;
+
+public class Enemy {
+  public String name;
+  public int id;
+  public Stats s;
+}
--- Game/Game.java
+++ Game/Game.java
@@ -9,8 +9,12 @@ public class Game {
     //Player - stats, inven
     //Player interactor - modifies player's stats and inven
     //Game state - explore, battle, pause, game over
+
+    //Make a player
     Player p1 = new Player();
+    int[] pLocation = new int[2];
     ConsoleUserInterface cui = new ConsoleUserInterface();
+
   }
 
 }

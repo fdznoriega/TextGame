fe42821055fd3186faa19d0f2c2e22d0c21a510f
fdznoriega <andresfdz7@gmail.com>
2019-09-19 13:40:21 -0400

    Drafted stats interactor; working toward bringing all together into player interactor

--- Domain/Interactors/PlayerInteractor.java
+++ Domain/Interactors/PlayerInteractor.java
@@ -6,6 +6,7 @@ import Domain.Interactors.InventoryInteractor;
 
 //player has stats and inventory which are manipulated by other classes
 //pInteractor holds these other interactors and is a "manager"
+//first large boi class!
 public class PlayerInteractor /**implements IPlayerInteractor **/ {
 
   public Player p;
--- Domain/Interactors/StatsInteractor.java
+++ Domain/Interactors/StatsInteractor.java
@@ -4,7 +4,21 @@ package Domain.Interactors;
 import Domain.Entities.Stats;
 import Domain.Interfaces.IStatsInteractor;
 
-public class StatsInteractor /** implements IStatsInteractor **/ {
+public class StatsInteractor /* implements IStatsInteractor */ {
   public Stats s;
   //public IStatsInteractorOut sOut;
+
+  /* methods to implement
+  void updateAll(Stats s);
+  void updateCurrentHp();
+  void updateMaxHp();
+  void updateAttack(int att);
+  void updateDefense(int def);
+  void updateSpeed(int spd);
+  void updateXp(int x);
+  void updateLvl(int l);
+  void updateGold(int g);
+  */
+
+
 }

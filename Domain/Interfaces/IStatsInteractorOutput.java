56a068e46efca61d8e873c0513948c80b89b6af7
fdznoriega <andresfdz7@gmail.com>
2019-09-27 09:52:23 -0400

    Pondering abstractness of IStatsInteractorOutput

--- Domain/Interfaces/IStatsInteractorOutput.java
+++ Domain/Interfaces/IStatsInteractorOutput.java
@@ -2,10 +2,24 @@ package Domain.Interfaces;
 
 //import...
 
+/* Note: Stats are currentHp, maxHp, attack, defense, speed, lvl, xp, gold */
+
 public interface IStatsInteractorOutput {
-  void showSetSuccess();
-  void showSetFailure();
-  void showIncrease();
-  void showDecrease();
-  void showLevelUp();
+  void showUpdateSuccess();
+  void showUpdateFailure();
+  //show increase of individual stats.
+  //void showIncrease...
+  //can i make this more abstract?
+  void showIncreaseCurrentHp();
+  void showIncreaseMaxHp();
+  void showIncreaseAttack();
+  void showIncreaseDefense();
+  void showIncreaseSpeed();
+  void showIncreaseLevel();
+  void showIncreaseExp();
+  void showIncreaseGold();
+  //show decrease of individual stats.
+  //void showDecrease...
+
+
 }

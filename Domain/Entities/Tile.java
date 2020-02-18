2ff9727c8ccb503d2a34fea5bec1516a3387dcdd
fdznoriega <andresfdz7@gmail.com>
2020-02-17 23:51:40 -0500

    Added Tiles

---
+++ Domain/Entities/Tile.java
@@ -0,0 +1,20 @@
+package Domain.Entities;
+
+// the maze is composed of tiles.
+// current tile IDs:
+// 0 - Wall
+// 1 - Spawn
+// 2 - Walkable
+// 3 - Treasure
+// 4 - Refresh
+// 5 - Enemy
+// 6 - Boss
+// there should be tile types!
+// environment type -> path, wall
+// treasure type -> randomized treasure specific treasure)
+// enemy type -> boss, enemy
+
+public class Tile {
+  TileType type;
+  int id;
+}
--- Domain/GameDirector.java
+++ Domain/GameDirector.java
@@ -9,15 +9,37 @@ import Domain.Entities.Direction;
 public class GameDirector {
 
   public Player player;
+  public int[] location; //player's location on the board
   public Maze maze;
   public PlayerInteractor pInteractor;
   public MazeInteractor mInteractor;
   public IGameDirectorInput input;
   public IGameDirectorOutput output;
 
+  // set player location
+  private void setInitialLocation() {
+    this.location = mInteractor.findSpawn();
+  }
+
   // movement calls the player can make
   public void movePlayer(Direction d) {
     //uses maze interactor to move player in desired direction
+    if(d.equals(Direction.North)) {
+      //check if null or wall
+    }
+    else if(d.equals(Direction.South)) {
+
+    }
+    else if(d.equals(Direction.East)) {
+
+    }
+    else if(d.equals(Direction.West)) {
+
+    }
+    else {
+      //output error
+      return;
+    }
   }
 
   // menu calls the player/user can make

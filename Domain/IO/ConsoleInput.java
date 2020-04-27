0ddadf57cc0d70eb8b14db5a74c69c567fccc68b
fdznoriega <andresfdz7@gmail.com>
2020-04-27 11:26:56 -0400

    Input investigation

--- Domain/IO/ConsoleInput.java
+++ Domain/IO/ConsoleInput.java
@@ -6,23 +6,21 @@ import Domain.Interfaces.IGameDirectorInput;
 
 public class ConsoleInput implements IGameDirectorInput {
 
+  public String text;
+
   // asks the user for an input
-  public void request() {
-    String text = "";
+  public void fetch() {
+    String t = "";
     Scanner s = new Scanner(System.in);
-    text = s.nextLine();
-    text = format(text);
-
-  }
-
-  // make input into the type of input we want
-  private String format(String s) {
-    return s.toLowerCase();
+    t = s.nextLine().toLowerCase(); // get and format
+    this.text = t;
   }
 
   // testing
   public static void main(String[] args) {
     ConsoleInput c = new ConsoleInput();
+    c.fetch();
+    System.out.println(c.text);
 
   }
 
